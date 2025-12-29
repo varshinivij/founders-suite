d90d91028fba9ecc9ed415aed62622b8c3eb0fd3
Varshini Vijay <varshinivijay05@gmail.com>
2025-12-29 18:01:09 +0530

    functional scraper

--- crawler.py
+++ crawler.py
@@ -5,11 +5,11 @@ MAX_DEPTH = 10
 
 #Extract links given a frontier of web pages
 def extract_links(frontier:list) -> list:
-    urls = []
+    urls = [] 
     depth = 0
     while depth < MAX_DEPTH and frontier:
         try: 
-            url = urls.pop(0)
+            url = frontier.pop(0)
             response = requests.get(url)
             if response.status_code != 200:
                 raise Exception("Invalid response")
@@ -23,35 +23,23 @@ def extract_links(frontier:list) -> list:
 
             for link_el in link_elements:
                 raw_url = link_el["href"]
-                urls.append(absolute_url(raw_url))
+                abs_url = absolute_url(url, raw_url)
+                frontier.append(abs_url)
+                urls.add(abs_url)
             
             depth += 1
             
         except Exception as e:
             print(f"Error: {e}")
+    
+    return urls
 
 #Convert a raw url to an absolute url
-def absolute_url(raw_url):
-    
-
+def absolute_url(target_url, url):
+    if not url.startswith("http"):
+        url = requests.compat.urljoin(target_url, url)
+    return url
 
 if __name__ == "__main__":
-    #extract_links(["https://www.w3schools.com/html/html_examples.html"])
-    # Import Beautiful Soup
-
-    # Initialize the object with a HTML page
-    soup = BeautifulSoup('''
-        
-            <h1 class="hello"> Heading 1 </h1>
-            <h1 class="welcome"> Heading 2 </h1>
-        
-        ''', "html.parser")
-
-    # Get the whole h2 tag
-    tag = soup.select("h1")
-
-    # Get the attribute
-    #attribute = tag.attrs
-
-    # Print the output
-    print(tag)
+    print(extract_links(["https://books.toscrape.com/"]))
+    
